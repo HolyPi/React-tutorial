7cabaf235a6e6e6d354ba9da3009640b48caff0c
HolyPi <joanelly2018@gmail.com>
2020-11-18 19:18:54 -0500

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import  './Footer.css';
+
+function Footer() {
+    return (
+      <div className="Footer">
+        <p> Joanelly Cruz copyright 2020 </p> 
+      </div>
+         )
+        }
+        
+      
+      export default Footer

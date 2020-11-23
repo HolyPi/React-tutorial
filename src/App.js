ddc850d2a6dcf72e409bd34136c0f66d7932da7f
HolyPi <joanelly2018@gmail.com>
2020-11-23 15:47:54 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './About'
 import Title from './Title';
+import POPOSDetails from './POPOSDetails'
 import POPOSList from './POPOSList';
 import Footer from './Footer'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Title />
-      <POPOSList />
-      <Footer />
-    </div>
+  <Router>
+  <div className="App">
+  <Route path="/details/:id" component={POPOSDetails} />
+    <Title />
+    <Route exact path="/" component={POPOSList}/>
+    <Route path="/about" component={About} />
+  </div>
+  </Router>
   );
 }
 
